15dd7f5409a51b03ce8f95eb9b03a933dd224623
shorinjimonk <bevinssmith@gmail.com>
2021-05-14 14:35:47 -0400

    Update BookForm.vue

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     <div>
       <div>
          <label for="finishedReading">
