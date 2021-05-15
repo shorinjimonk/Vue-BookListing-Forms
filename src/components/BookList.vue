bb4683a4b9ba3c8ba9dba26de66686ceec9f12a3
shorinjimonk <bevinssmith@gmail.com>
2021-05-15 17:38:12 -0400

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -15,9 +15,10 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
-import _ from "lodash";
+
 
 export default {
   name: "BookList",
